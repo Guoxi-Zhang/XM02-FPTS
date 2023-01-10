176c0ec8b2bcbfee5e145d26e0560c10189fda4b
melody0123 <88380000+melody0123@users.noreply.github.com>
2023-01-11 04:10:08 +0800

    完成web端重置密码的邮件发送前端

--- fpts-admin/src/main/resources/static/ruoyi/resetPassword.js
+++ fpts-admin/src/main/resources/static/ruoyi/resetPassword.js
@@ -9,33 +9,31 @@ $(function() {
 
 $.validator.setDefaults({
     submitHandler: function() {
-    	register();
+    	submitEmail();
     }
 });
 
-function register() {
+function submitEmail() {
     $.modal.loading($("#btnSubmit").data("loading"));
-    var username = $.common.trim($("input[name='username']").val());
-    var password = $.common.trim($("input[name='password']").val());
+    var email = $.common.trim($("input[name='email']").val());
     var validateCode = $("input[name='validateCode']").val();
     $.ajax({
         type: "post",
-        url: ctx + "register",
+        url: ctx + "resetPassword/sendCode",
         data: {
-            "loginName": username,
-            "password": password,
+            "email": email,
             "validateCode": validateCode
         },
         success: function(r) {
             if (r.code == web_status.SUCCESS) {
-            	layer.alert("<font color='red'>恭喜你，您的账号 " + username + " 注册成功！</font>", {
+            	layer.alert("<font color='red'>重置密码邮件发送成功，请前往您的邮箱查看！</font>", {
             	    icon: 1,
             	    title: "系统提示"
             	},
             	function(index) {
             	    //关闭弹窗
             	    layer.close(index);
-            	    location.href = ctx + 'login';
+            	    location.href = ctx + 'resetPassword/setNewPassword';
             	});
             } else {
             	$.modal.closeLoading();
@@ -49,33 +47,17 @@ function register() {
 
 function validateRule() {
     var icon = "<i class='fa fa-times-circle'></i> ";
-    $("#registerForm").validate({
+    $("#submitEmailForm").validate({
         rules: {
-            username: {
+            email: {
                 required: true,
-                minlength: 2
-            },
-            password: {
-                required: true,
-                minlength: 5
-            },
-            confirmPassword: {
-                required: true,
-                equalTo: "[name='password']"
+                email: true
             }
         },
         messages: {
-            username: {
-                required: icon + "请输入您的用户名",
-                minlength: icon + "用户名不能小于2个字符"
-            },
-            password: {
-            	required: icon + "请输入您的密码",
-                minlength: icon + "密码不能小于5个字符",
-            },
-            confirmPassword: {
-                required: icon + "请再次输入您的密码",
-                equalTo: icon + "两次密码输入不一致"
+            email: {
+                required: icon + "请输入您提供的邮箱",
+                email: icon + "请输入格式正确的邮箱"
             }
         }
     })
