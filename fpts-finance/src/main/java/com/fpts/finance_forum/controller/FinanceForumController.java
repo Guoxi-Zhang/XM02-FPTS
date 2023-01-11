25073e175beb435f9b7bbf452dd7eca42db17e8d
liyicheng1 <96060811+liyicheng1@users.noreply.github.com>
2023-01-11 18:21:29 +0800

    这是一个疑似有问题的小程序论坛模块接口
    看我看我，我宣布个事儿，我是个傻逼！

--- fpts-finance/src/main/java/com/fpts/finance_forum/controller/FinanceForumController.java
+++ fpts-finance/src/main/java/com/fpts/finance_forum/controller/FinanceForumController.java
@@ -4,15 +4,12 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 import com.fpts.finance_collection.domain.FinanceCollection;
+import com.fpts.finance_warehouse.domain.FinanceWarehouse;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import com.fpts.common.annotation.Log;
 import com.fpts.common.enums.BusinessType;
 import com.fpts.finance_forum.domain.FinanceForum;
@@ -190,5 +187,36 @@ public class FinanceForumController extends BaseController
         mmap.put("pIdList", pIdList);
         mmap.put("cntList", cntList);
         return prefix + "/forumchart";
+    }
+
+    /** 小程序列表 */
+    @RequestMapping(value = "/wxGet/{Navtab}", method = RequestMethod.POST)
+    @ResponseBody
+    public List<FinanceForum> get(FinanceForum financeForum, @PathVariable("Navtab") String tab){
+        List<FinanceForum> list = financeForumService.selectFinanceForumList(financeForum);
+        List<FinanceForum> ansList = new ArrayList<>();
+
+        for(FinanceForum t: list){
+            String content = t.getContent();
+            if(content.contains("<p>")){
+                t.setContent(t.getContent().replace("<p>", ""));
+                System.out.println(content.replace("<p>", ""));
+            }
+            if(content.contains("</p>")){
+                t.setContent(t.getContent().replace("</p>", ""));
+            }
+            if(tab.equals("0") ){
+                ansList.add(t);
+            }
+        }
+        System.out.println(ansList.toString());
+        return ansList;
+    }
+
+    @RequestMapping(value = "/wxEdit/{id}", method = RequestMethod.POST)
+    @ResponseBody
+    public FinanceForum wxEdit( @PathVariable("id") String id){
+        FinanceForum item = financeForumService.selectFinanceForumById(Long.valueOf(id));
+        return item;
     }
 }
